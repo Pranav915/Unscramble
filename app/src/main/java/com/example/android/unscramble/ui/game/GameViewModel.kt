bf575f7cf525a292a0196f4b724349b25b496dd0
Pranav Patil <patilpranav915@gmail.com>
2021-04-29 09:37:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    init {
+        Log.d("GameFragment", "GameViewModel Created!")
+        getNextWord()
+    }
+
+    private var _score = 0
+    val score:Int
+    get() = _score
+    private var currentWordCount = 0
+    private var wordsList:MutableList<String> = mutableListOf()
+    private lateinit var currentWord:String
+    private lateinit var _currentScrambledWord:String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment","GameViewModel Destroyed!")
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+    fun nextWord():Boolean{
+
+        return if (currentWordCount< MAX_NO_OF_WORDS){
+            getNextWord()
+            true
+        }else false
+
+    }
+
+}
